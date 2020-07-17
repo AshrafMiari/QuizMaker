9a021eead8400cbb96598848cb1fca98770ad456
Ashraf-Miari <61672785+Ashraf-Miari@users.noreply.github.com>
2020-07-17 21:20:45 +1000

    started newGame within console

--- src/gui/NewGame.java
+++ src/gui/NewGame.java
@@ -2,13 +2,67 @@ package gui;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import appLogic.*;
 
 public class NewGame {
 	
-	public List<Player> players = new ArrayList<Player>();
-	public List<Question> questions = new ArrayList<Question>();
+	private List<Player> players;
+	private List<Question> questions;
+	private int numOfPlayers = 0;
+	private int numOfQuestions = 0;
+
+	Scanner sc= new Scanner(System.in);
 	
+	
+	public NewGame() {
+		players = new ArrayList<Player>();
+		questions = new ArrayList<Question>();
+	}
+	
+	public void start() {
+		
+		boolean gameStarted = false;
+		while(gameStarted == false) {
+			System.out.println("1. Add Player \n" +
+					   "2. Add Question \n" +
+					   "3. Start Game \n" + 
+					   "4. Quit");
+			int input = sc.nextInt();
+			sc.nextLine();
+			if(input == 1) {
+				addPlayer();
+			}else if(input == 2) {
+				addQuestion();
+			}else if(input == 3) {
+				System.out.println("To be implemented");
+			}else if(input == 4) {
+				System.exit(0);
+			}else {
+				System.out.println("Please enter a valid Command");
+			}
+		}
+		sc.close();
+	}
+	
+	private void addPlayer() {
+		System.out.print("Enter Player Name: ");
+		String name = sc.nextLine();
+		Player newPlayer = new Player(name, 0, numOfPlayers);
+		players.add(newPlayer);
+		numOfPlayers++;
+		
+	}
+	
+	private void addQuestion() {
+		System.out.print("Enter Question: ");
+		String question = sc.nextLine();
+		System.out.print("Enter Answer: ");
+		String answer = sc.nextLine();
+		Question newQuestion = new Question(question, answer, numOfQuestions);
+		questions.add(newQuestion);
+		numOfQuestions++;
+	}
 	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,13 @@
 package main;
 
+import gui.NewGame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		NewGame game = new NewGame();
+		game.start();
 
 	}
 
