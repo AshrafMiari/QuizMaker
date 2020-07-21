6436d3a5820720af19c5e7f2eb44cb3448d27e8b
Ashraf-Miari <61672785+Ashraf-Miari@users.noreply.github.com>
2020-07-21 21:16:09 +1000

    finalised review questions page

--- src/gui/ReviewQuestions.java
+++ src/gui/ReviewQuestions.java
@@ -6,17 +6,19 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JTextPane;
 
 public class ReviewQuestions implements ActionListener{
 
 	private JFrame frame = new JFrame("Review Questions");
 	private JButton next = new JButton("Next");
 	private JButton prev = new JButton("Prev");
-	private JLabel question = new JLabel("This is a question");
-	private JLabel answer = new JLabel("this is a Answer");
+	private JTextPane question = new JTextPane();
+	private JTextPane answer = new JTextPane();
 	
 	private NewGame settings;
+	
+	private int currentQuestionNumber = 0;
 	
 	public ReviewQuestions(NewGame s) {
 		settings = s;
@@ -30,19 +32,47 @@ public class ReviewQuestions implements ActionListener{
 		prev.setBounds(20, 400, 100, 50);
 		prev.addActionListener(this);
 		frame.add(question);
-		question.setBounds(100, 10, 1000, 100);
+		question.setBounds(100, 10, 400, 100);
 		question.setText(settings.questions.get(0).getContents());
-		question.setFont(new Font(null, Font.BOLD, 40));
+		question.setFont(new Font(null, Font.BOLD, 20));
+		question.setOpaque(false);
 		frame.add(answer);
 		answer.setText(settings.questions.get(0).getAnswer());
-		answer.setBounds(100, 300, 1000, 100);
+		answer.setBounds(100, 300, 400, 100);
 		answer.setFont(new Font(null, Font.PLAIN, 20));
+		answer.setOpaque(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true); 
 	}
 	
+	private void goToNextQuestion() {
+		if(currentQuestionNumber != settings.numOfQuestions -1) {
+			question.setText(settings.questions.get(currentQuestionNumber + 1).getContents());
+			answer.setText(settings.questions.get(currentQuestionNumber + 1).getAnswer());
+			currentQuestionNumber++;
+			question.paintImmediately(question.getVisibleRect());
+			answer.paintImmediately(answer.getVisibleRect());
+			frame.repaint();
+		}
+	}
+	
+	private void goToPrevQuestion() {
+		if(currentQuestionNumber != 0) {
+			question.setText(settings.questions.get(currentQuestionNumber - 1).getContents());
+			answer.setText(settings.questions.get(currentQuestionNumber - 1).getAnswer());
+			question.paintImmediately(question.getVisibleRect());
+			answer.paintImmediately(answer.getVisibleRect());
+			frame.repaint();
+			currentQuestionNumber--;
+		}
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		if(e.getSource() == next) {
+			goToNextQuestion();
+		}else if(e.getSource() == prev) {
+			goToPrevQuestion();
+		}
 	}
 }
